57be2c7030ecfbeaba4d44a1b69447ed3ea13562
Niedson Emanoel <niedsonemanoeltbm@gmail.com>
2023-12-20 12:20:32 -0300

    sss

--- simulate.py
+++ simulate.py
@@ -35,46 +35,48 @@ def encontrar_theta_max(a, b, c, x):
 st.set_page_config(layout='wide', page_title='Enemaster.app', initial_sidebar_state="expanded", page_icon="🧊",    menu_items={
         'About': "# Feito por *enemaster.app*"
     })
+def main():
+    st.sidebar.markdown(f'<img width="100%" src="https://raw.githubusercontent.com/NiedsonEmanoel/NiedsonEmanoel/main/enem/An%C3%A1lise%20de%20Itens/OrdenarPorTri/natureza/EneMaster.png">',unsafe_allow_html=True)
+    st.sidebar.markdown(f"<br><hr>",unsafe_allow_html=True)
+    Nome = st.sidebar.text_input('Nome')
+    email = st.sidebar.text_input('Email')
 
-st.sidebar.markdown(f'<img width="100%" src="https://raw.githubusercontent.com/NiedsonEmanoel/NiedsonEmanoel/main/enem/An%C3%A1lise%20de%20Itens/OrdenarPorTri/natureza/EneMaster.png">',unsafe_allow_html=True)
-st.sidebar.markdown(f"<br><hr>",unsafe_allow_html=True)
-Nome = st.sidebar.text_input('Nome')
-email = st.sidebar.text_input('Email')
-
-st.sidebar.markdown(f"<hr>",unsafe_allow_html=True)
-uploaded_file = st.sidebar.file_uploader("Envie o arquivo do Simulado", type=['csv'])
-if uploaded_file is not None:
-    dItens = pd.read_csv(uploaded_file, encoding='utf-8', decimal=',')
-    a, b, c = zip(*dItens[['NU_PARAM_A', 'NU_PARAM_B', 'NU_PARAM_C']].values.tolist())
-    total = len(a)
-    st.title("Simulado:")
-else:
-    st.title("Adicione o arquivo .csv do seu simulado ao lado.")
-
-anwers = []
-
-with st.form("form"):
-
+    st.sidebar.markdown(f"<hr>",unsafe_allow_html=True)
+    uploaded_file = st.sidebar.file_uploader("Envie o arquivo do Simulado", type=['csv'])
     if uploaded_file is not None:
-        for i in dItens.index:
-            l = dItens.loc[i, "imagAPI"]
-            st.markdown(f'<img src="{l}">',unsafe_allow_html=True)
-            st.markdown(f"<br>",unsafe_allow_html=True)
-            Resposta = st.radio('Resposta', ['A', 'B', 'C', 'D', 'E'], horizontal=True, key=dItens.loc[i, "CO_ITEM"])
-            if dItens.loc[i, "TX_GABARITO"] == Resposta:
-                anwers.append(1)
-            else:
-                anwers.append(0)
-    
-            st.markdown(f"<hr>",unsafe_allow_html=True)
+        dItens = pd.read_csv(uploaded_file, encoding='utf-8', decimal=',')
+        a, b, c = zip(*dItens[['NU_PARAM_A', 'NU_PARAM_B', 'NU_PARAM_C']].values.tolist())
+        total = len(a)
+        st.title("Simulado:")
+    else:
+        st.title("Adicione o arquivo .csv do seu simulado ao lado.")
 
-        submitted = st.form_submit_button("Concluir!", type="primary")
-        if submitted:
-            x = np.array(anwers)
-            with st.spinner("Estimando sua nota TRI..."):
-                time.sleep(2)
-                theta_max_list = encontrar_theta_max(a, b, c, x)
-            tri = round(theta_max_list[0],2)
-            st.success('Sua nota aproximada é: '+str(tri), icon="✅")
-            st.balloons()
+    anwers = []
+
+    with st.form("form"):
+
+        if uploaded_file is not None:
+            for i in dItens.index:
+                l = dItens.loc[i, "imagAPI"]
+                st.markdown(f'<img src="{l}">',unsafe_allow_html=True)
+                st.markdown(f"<br>",unsafe_allow_html=True)
+                Resposta = st.radio('Resposta', ['A', 'B', 'C', 'D', 'E'], horizontal=True, key=dItens.loc[i, "CO_ITEM"])
+                if dItens.loc[i, "TX_GABARITO"] == Resposta:
+                    anwers.append(1)
+                else:
+                    anwers.append(0)
+        
+                st.markdown(f"<hr>",unsafe_allow_html=True)
+
+            submitted = st.form_submit_button("Concluir!", type="primary")
+            if submitted:
+                x = np.array(anwers)
+                with st.spinner("Estimando sua nota TRI..."):
+                    time.sleep(2)
+                    theta_max_list = encontrar_theta_max(a, b, c, x)
+                tri = round(theta_max_list[0],2)
+                st.success('Sua nota aproximada é: '+str(tri), icon="✅")
+                st.balloons()
+if __name__ == "__main__":
+    main()
             
